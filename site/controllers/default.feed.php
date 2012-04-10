cc14a107435db96ba20f16f932187ee8d64841b5
Sylvain Philip <contact@sphilip.com>
2012-04-10 00:17:29 +0000

    Fix mispelled translations, migrate property view feed, fix menuItem in module JEA search

---
+++ site/controllers/default.feed.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This file is part of Joomla Estate Agency - Joomla! extension for real estate agency
+ *
+ * @version     $Id$
+ * @package     Joomla.Site
+ * @subpackage  com_jea
+ * @copyright   Copyright (C) 2008 - 2012 PHILIP Sylvain. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+jimport('joomla.application.component.controller');
+
+/**
+ * Default feed controller class.
+ *
+ * @package     Joomla.Site
+ * @subpackage  com_jea
+ */
+class JeaControllerDefault extends JController
+{
+    protected $default_view = 'properties';
+
+}
--- site/views/properties/view.feed.php
+++ site/views/properties/view.feed.php
@@ -1,72 +1,61 @@
 <?php
 /**
- * This file is (not quite yet) a part of Joomla Estate Agency - Joomla! extension for real estate agency
+ * This file is part of Joomla Estate Agency - Joomla! extension for real estate agency
  *
  * @version     $Id$
- * @package     Jea.site
- * @copyright   Copyright (C) 2010 Ændrew Rininsland. Based on work by PHILIP Sylvain. All rights reserved.
- * @license     GNU/GPL, see LICENSE.php
- * Joomla Estate Agency is free software. This version may have been modified pursuant to the
- * GNU General Public License, and as distributed it includes or is derivative
- * of works licensed under the GNU General Public License or other free or open
- * source software licenses.
- *
+ * @package     Joomla.Site
+ * @subpackage  com_jea
+ * @copyright   Copyright (C) 2008 - 2012 PHILIP Sylvain. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-
-
 
 // Check to ensure this file is included in Joomla!
 
 defined('_JEXEC') or die();
 
-require_once JPATH_COMPONENT.DS.'view.php';
+jimport('joomla.application.component.view');
 
-class JeaViewProperties extends JeaView 
+class JeaViewProperties extends JView
 {
-    
+
     function display($tpl = null)
     {
-        global $mainframe;
+        $app = JFactory::getApplication();
 
-        $document    =& JFactory::getDocument();
-        $params =& $mainframe->getParams();
-        
-        $siteEmail = $mainframe->getCfg('mailfrom');
-                
-        $document->link = JRoute::_('index.php?option=com_jea&view=properties');
+        $document    = JFactory::getDocument();
+        $params = $app->getParams();
 
-        JRequest::setVar('limit', $mainframe->getCfg('feed_limit'));
-        
-        $res = $this->get('properties');
-        
-        foreach ( $res['rows'] as $row ) {
+        $document->link  = JRoute::_('index.php?option=com_jea&view=properties');
+
+        JRequest::setVar('limit', $app->getCfg('feed_limit'));
+
+        $rows = $this->get('Items');
+
+        foreach ($rows as $row ) {
 
             if(empty($row->title)) {
-    		    $title = ucfirst( JText::sprintf('PROPERTY TYPE IN TOWN',
-    		    $this->escape($row->type), $this->escape($row->town)));
-    		} else {
-    		    // strip html from feed item title
-    		    $title = $this->escape( $row->title );
-    		}
-            
-            
+                $title = ucfirst( JText::sprintf('COM_JEA_PROPERTY_TYPE_IN_TOWN',
+                $this->escape($row->type), $this->escape($row->town)));
+            } else {
+                // strip html from feed item title
+                $title = $this->escape($row->title);
+            }
+
+            $row->slug = $row->alias ? ($row->id . ':' . $row->alias) : $row->id;
             // url link to article
-            $item->link = JRoute::_('index.php?view=properties&id='. $row->id);
- 
+            $item->link = JRoute::_('index.php?view=properties&id='. $row->slug);
+
             // strip html from feed item description text
             $description   = strip_tags($row->description);
-            $author        = "Author"; // soon, will get the author name
-                        
+
             // load individual item creator class
             $item = new JFeedItem();
             $item->title         = html_entity_decode($title);
-            $item->link         = $link;
-            $item->description     = $description;
-            $item->date            = $row->date_insert;
-            $item->category       = $row->type_id;
-            $item->author        = $author;
-            $item->authorEmail = $feedemail;
-            
+            $item->link          = $link;
+            $item->description   = $description;
+            $item->date          = $row->date_insert;
+            $item->category      = $row->type_id;
+
             // loads item info into rss array
             $document->addItem( $item );
         }
