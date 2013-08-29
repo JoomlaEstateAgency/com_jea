08b3cd438cde6e7137cdaa13e255cc9fe1fb9b85
Sylvain Philip <contact@sphilip.com>
2013-08-29 08:27:30 +0000

    Fix bug to load large images

--- site/views/property/tmpl/default_gallery.php
+++ site/views/property/tmpl/default_gallery.php
@@ -15,37 +15,39 @@ defined('_JEXEC') or die('Restricted access');
 if (!is_array($this->row->images)) {
     return ;
 }
+JHTML::_('behavior.modal');
 
 $mainImage = $this->row->images[0];
 
 $script=<<<EOB
 function updatePreviewInfos(){
-	var imgTitle       = document.id('jea-preview-img').getProperty('alt');
-	var imgDescription = document.id('jea-preview-img').getProperty('title');
-	document.id('jea-preview-title').empty();
-	document.id('jea-preview-description').empty();
-	if (imgTitle) {
-		document.id('jea-preview-title').set('text', imgTitle);
+    var imgTitle       = document.id('jea-preview-img').getProperty('alt');
+    var imgDescription = document.id('jea-preview-img').getProperty('title');
+    document.id('jea-preview-title').empty();
+    document.id('jea-preview-description').empty();
+    if (imgTitle) {
+        document.id('jea-preview-title').set('text', imgTitle);
     }
     if(imgDescription) {
-		document.id('jea-preview-description').set('text', imgDescription);
+        document.id('jea-preview-description').set('text', imgDescription);
     }
 };
 
 window.addEvent('domready', function() {
-	$$('a.jea-thumbnails').each(function(el) {
-		el.addEvent('click', function(e) {
-    		new Event(e).stop();
-    		if(document.id('jea-preview-img')){
-    			document.id('jea-preview-img').set({
-    				'alt'  : this.getElement('img').getProperty('alt'),
-    				'title': this.getElement('img').getProperty('title'),
-    				'src'  :  this.getProperty('href')
-    			});
-				updatePreviewInfos();
-			}
-		});
-	});
+    $$('a.jea-thumbnails').each(function(el) {
+        el.addEvent('click', function(e) {
+            new Event(e).stop();
+            if(document.id('jea-preview-img')){
+                document.id('jea-preview-img').set({
+                    'alt'  : this.getElement('img').getProperty('alt'),
+                    'title': this.getElement('img').getProperty('title'),
+                    'src'  :  this.getProperty('rel')
+                });
+                document.id('jea-preview-img').getParent().set('href', this.getProperty('href'));
+                updatePreviewInfos();
+            }
+        });
+    });
 });
 EOB;
 
@@ -59,7 +61,7 @@ $img_height = $this->params->get('thumb_medium_height', 400);
 <div class="clr" ></div>
 
 <div id="jea-gallery-preview" class="<?php echo $gallery_orientation ?>">
-<a href="<?php echo $mainImage->URL ?>" >
+<a href="<?php echo $mainImage->URL ?>" class="modal">
       <img src="<?php echo $mainImage->mediumURL ?>" 
            id="jea-preview-img"
            alt="<?php echo $mainImage->title ?>" 
@@ -73,7 +75,7 @@ $img_height = $this->params->get('thumb_medium_height', 400);
      class="<?php echo $gallery_orientation ?>"
      style="<?php echo $gallery_orientation == 'horizontal' ? 'width:'.$img_width.'px' : 'max-height:'.$img_height.'px' ?>">
      <?php foreach($this->row->images as $image) : ?>
-     <a class="jea-thumbnails" href="<?php echo $image->mediumURL?>" >
+     <a class="jea-thumbnails" rel="<?php echo $image->mediumURL?>" href="<?php echo $image->URL?>" >
      <img src="<?php echo $image->minURL ?>" 
            alt="<?php echo $image->title ?>" 
            title="<?php echo $image->description  ?>" /></a><br />
