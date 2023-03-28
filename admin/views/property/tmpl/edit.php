0c405dfffb056d9c958a9970374d0d363cf9f96e
ilhooq <contact@sphilip.com>
2023-03-28 15:12:18 +0200

    Use tabs in edit form

--- admin/views/property/tmpl/edit.php
+++ admin/views/property/tmpl/edit.php
@@ -11,11 +11,12 @@
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Factory;
-use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\CMS\Language\Text;
-use Joomla\CMS\Plugin\PluginHelper;
-use Joomla\CMS\Router\Route;
 use Joomla\Event\Event;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Layout\LayoutHelper;
+use Joomla\CMS\Plugin\PluginHelper;
 
 /**
  * @var $this JeaViewProperty
@@ -42,28 +43,27 @@ HTMLHelper::_('formbehavior.chosen', '#localization select');
     method="post" id="adminForm"
     class="form-validate" enctype="multipart/form-data">
 
-  <div class="card">
-    <div class="card-body">
-      <div class="form-inline form-inline-header">
-          <?php echo $this->form->renderField('title') ?>
-          <?php echo $this->form->renderField('ref') ?>
-          <?php echo $this->form->renderField('alias') ?>
-      </div>
+  <?php echo LayoutHelper::render('joomla.edit.title_alias', $this) ?>
+
+  <div class="main-card">
+    <?php echo HTMLHelper::_('uitab.startTabSet', 'property-tab', ['active' => 'general', 'recall' => true, 'breakpoint' => 768]) ?>
+    <?php echo HTMLHelper::_('uitab.addTab', 'property-tab', 'general', Text::_('COM_JEA_CONFIG_GENERAL')) ?>
       <div class="row">
-        <div class="col-8">
+        <div class="col-lg-9">
           <fieldset class="adminform">
-              <?php echo $this->form->renderField('transaction_type') ?>
-              <?php echo $this->form->renderField('type_id') ?>
-              <?php echo $this->form->renderField('description') ?>
+            <?php echo $this->form->renderField('ref') ?>
+            <?php echo $this->form->renderField('transaction_type') ?>
+            <?php echo $this->form->renderField('type_id') ?>
+            <?php echo $this->form->renderField('description') ?>
           </fieldset>
         </div>
-        <div class="col-4">
+        <div class="col-lg-3">
           <fieldset class="form-vertical">
-              <?php echo $this->form->renderField('published') ?>
-              <?php echo $this->form->renderField('featured') ?>
-              <?php echo $this->form->renderField('access') ?>
-              <?php echo $this->form->renderField('language') ?>
-              <?php echo $this->form->renderField('slogan_id') ?>
+            <?php echo $this->form->renderField('published') ?>
+            <?php echo $this->form->renderField('featured') ?>
+            <?php echo $this->form->renderField('access') ?>
+            <?php echo $this->form->renderField('language') ?>
+            <?php echo $this->form->renderField('slogan_id') ?>
           </fieldset>
 
           <?php echo HTMLHelper::_('bootstrap.startAccordion', 'property-sliders', ['active' => 'picture-pane']) ?>
@@ -85,64 +85,43 @@ HTMLHelper::_('formbehavior.chosen', '#localization select');
           <?php echo HTMLHelper::_('bootstrap.endAccordion') ?>
         </div>
       </div>
-    </div>
-  </div>
+    <?php echo HTMLHelper::_('uitab.endTab') ?>
 
-  <div class="card mt-4">
-    <div class="card-header">
-      <h2><?php echo Text::_('COM_JEA_FINANCIAL_INFORMATIONS') ?></h2>
-    </div>
-    <div class="card-body">
-        <?php foreach ($this->form->getFieldset('financial_informations') as $field) echo $field->renderField() ?>
-    </div>
-  </div>
+    <?php echo HTMLHelper::_('uitab.addTab', 'property-tab', 'financial', Text::_('COM_JEA_FINANCIAL_INFORMATIONS')) ?>
+      <?php foreach ($this->form->getFieldset('financial_informations') as $field) echo $field->renderField() ?>
+    <?php echo HTMLHelper::_('uitab.endTab') ?>
 
-  <div class="card mt-4">
-    <div class="card-header">
-      <h2><?php echo Text::_('COM_JEA_LOCALIZATION') ?></h2>
-    </div>
-    <div class="card-body" id="localization">
-        <?php foreach ($this->form->getFieldset('localization') as $field) echo $field->renderField() ?>
-    </div>
-  </div>
-
-  <div class="card mt-4">
-    <div class="card-header">
-      <h2><?php echo Text::_('COM_JEA_DETAILS') ?></h2>
-    </div>
-    <div class="card-body">
-        <?php foreach ($this->form->getFieldset('details') as $field) echo $field->renderField() ?>
-    </div>
-  </div>
-
-  <div class="card mt-4">
-    <div class="card-header">
-      <h2><?php echo Text::_('COM_JEA_AMENITIES') ?></h2>
-    </div>
-    <div class="card-body">
-        <?php echo $this->form->getInput('amenities') ?>
-    </div>
-  </div>
-
-  <div class="card mt-4">
-    <div class="card-header">
-      <h2><?php echo Text::_('COM_JEA_PUBLICATION_INFO') ?></h2>
-    </div>
-    <div class="card-body">
-        <?php foreach ($this->form->getFieldset('publication') as $field) echo $field->renderField() ?>
-    </div>
-  </div>
-
-  <?php if ($this->canDo->get('core.admin')) : ?>
-    <div class="card mt-4">
-      <div class="card-header">
-        <h2><?php echo Text::_('COM_JEA_FIELDSET_RULES') ?></h2>
+    <?php echo HTMLHelper::_('uitab.addTab', 'property-tab', 'localization', Text::_('COM_JEA_LOCALIZATION')) ?>
+      <div id="localization">
+          <?php foreach ($this->form->getFieldset('localization') as $field) echo $field->renderField() ?>
       </div>
-      <div class="card-body">
-          <?php echo $this->form->getInput('rules') ?>
+    <?php echo HTMLHelper::_('uitab.endTab') ?>
+
+    <?php echo HTMLHelper::_('uitab.addTab', 'property-tab', 'details', Text::_('COM_JEA_DETAILS')) ?>
+      <?php foreach ($this->form->getFieldset('details') as $field) echo $field->renderField() ?>
+    <?php echo HTMLHelper::_('uitab.endTab') ?>
+
+    <?php echo HTMLHelper::_('uitab.addTab', 'property-tab', 'amenities', Text::_('COM_JEA_AMENITIES')) ?>
+      <div>
+        <fieldset class="options-form">
+          <legend><?php echo Text::_('COM_JEA_AMENITIES') ?></legend>
+          <?php echo $this->form->getInput('amenities') ?>
+        </fieldset>
       </div>
-    </div>
-  <?php endif ?>
+    <?php echo HTMLHelper::_('uitab.endTab') ?>
+
+    <?php echo HTMLHelper::_('uitab.addTab', 'property-tab', 'amenities', Text::_('COM_JEA_PUBLICATION_INFO')) ?>
+      <?php foreach ($this->form->getFieldset('publication') as $field) echo $field->renderField() ?>
+    <?php echo HTMLHelper::_('uitab.endTab') ?>
+
+    <?php if ($this->canDo->get('core.admin')) : ?>
+      <?php echo HTMLHelper::_('uitab.addTab', 'property-tab', 'rules', Text::_('COM_JEA_FIELDSET_RULES')) ?>
+        <?php echo $this->form->getInput('rules') ?>
+      <?php echo HTMLHelper::_('uitab.endTab') ?>
+    <?php endif ?>
+
+    <?php echo HTMLHelper::_('uitab.endTabSet') ?>
+  </div>
 
   <div>
     <input type="hidden" name="task" value=""/>
