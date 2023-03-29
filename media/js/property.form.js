eca00d2dc34c8c8a305afbd5c5e767f833f6faeb
ilhooq <contact@sphilip.com>
2023-03-29 09:46:39 +0200

    Fix amenities bugs

--- media/js/property.form.js
+++ media/js/property.form.js
@@ -26,61 +26,51 @@ function updateFeature(name, fieldId, language) {
 };
 
 function updateAmenities(language) {
-  //active option selected
-  var labels = document.getElementsByClassName('amenity');
-  var checkedLabels = Array();
-  //store active amenities & clear labels
-  for (let label of labels) {
-    var input = label.getElementsByTagName('input')[0];
-    if (input.checked) {
-      checkedLabels.push(input.value);
-    }
-  }
-  //remove current amenities
-  $('#amenities').empty();
 
-  $.ajax({
-    url: 'index.php',
-    data: {
+  const ulElement = $('#amenities');
+  var selected = [];
+
+  ulElement.find('.amenity input').each(function(index, input) {
+    if (input.checked) {
+      selected.push(input.value);
+    }
+  });
+
+  ulElement.empty();
+
+  $.getJSON('index.php', {
       'option': 'com_jea',
       'format': 'json',
       'task': 'features.get_list',
       'feature': 'amenity',
       'language': language
-    }
-  })
+    })
   .done(function (response) {
-    if (response) {
-
-      const ulElement = $('#amenities');
-
-      for (let i = 0; i < response.length; i++) {
-        const item = response[i];
-
-        const isActive = checkedLabels.findIndex(value => value == item.id) >= 0;
-        const activeClass = isActive ? 'active' : '';
-
-        const liElement = $(`<li class="amenity ${activeClass}"></li>`);
-
+    if (response.length) {
+      $.each(response, (i, item) => {
+        const isActive = selected.findIndex(value => value == item.id) >= 0;
+        const activeClass = isActive ? ' active' : '';
+        const liElement = $(`<li class="amenity${activeClass}"></li>`);
         const checkbox = $(
-            `<input class="am-input" type="checkbox" name="jform[amenities][]" id="jform_amenities${i}" value="${item.id}" checked="${isActive}" style="display: none;">`);
-
+            `<input class="am-input" type="checkbox" name="jform[amenities][]" id="jform_amenities${i}" value="${item.id}"${isActive ? ' checked="checked"' : ''}>`);
         const label = $(
-            `<label class="am-title" id="jform_amenities_label${i}" for="jform_amenities${i}">${item.value} (${item.id})</label>`);
+            `<label class="am-title" for="jform_amenities${i}">${item.value}</label>`);
 
-        label.on('click', function (event) {
-          if (checkbox.is(':checked')) {
-            liElement.removeClass('active');
-            checkbox.checked = false;
+        liElement.on('click', function () {
+          const input = $(this).find('input');
+
+          if (input.is(':checked')) {
+            $(this).removeClass('active');
+            input.prop('checked', false);
           } else {
-            liElement.addClass('active');
-            checkbox.checked = true;
+            $(this).addClass('active');
+            input.prop('checked', true);
           }
         });
 
         liElement.append(checkbox, label);
         ulElement.append(liElement);
-      }
+      });
     }
   });
 }
@@ -107,7 +97,7 @@ document.addEventListener('DOMContentLoaded', function () {
   $('#jform_language').on('change', function (event) {
 
     // show field alerts
-    $('#ajaxupdating').css('display', '');
+    $('#ajaxupdating').css('display', 'block');
     $('#jform_type_id').css('border', '1px solid ' + borderColor);
     $('#jform_condition_id').css('border', '1px solid ' + borderColor);
     $('#jform_heating_type').css('border', '1px solid ' + borderColor);
